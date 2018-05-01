30de3462be6a87e8b1d7d1f6de8eaa7e6fee4620
Jon Krazov <krazov@interia.pl>
2018-05-02 00:26:55 +0200

    Module generating arrays per need

---
+++ generator.js
@@ -0,0 +1,36 @@
+const { floor, random } = Math;
+
+const randomFromRange = (max) => floor(random() * max);
+
+const findUnique = (max, array) => {
+    do {
+        const x = randomFromRange(max * 10);
+
+        if (!array.includes(x)) {
+            return x;
+        }
+    }
+    while (true);
+};
+
+const fillVaried = (final, _1, _2, array) =>
+    (final.push(findUnique(array.length, final)), final);
+
+module.exports = {
+    generateRandom(length) {
+        return Array
+            .from({ length })
+            .reduce(fillVaried, []);
+    },
+
+    generateOrdered(length) {
+        return Array
+            .from({ length }, (_, index) => index);
+    },
+
+    generateReversed(length) {
+        return Array
+            .from({ length }, (_, index) => index)
+            .reverse();
+    },
+}
